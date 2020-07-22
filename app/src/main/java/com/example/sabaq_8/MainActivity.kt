e031a40cede01c847ccd9fc7911b4b45f0ab5a4f
Your Name <you@example.com>
2020-07-22 14:48:53 +0500

    Math Game oyini jaratildi

---
+++ app/src/main/java/com/example/sabaq_8/MainActivity.kt
@@ -0,0 +1,81 @@
+package com.example.sabaq_8
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Math.random
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    private val random= Random
+    private var res = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        getRightAnswer()
+    }
+
+
+
+    fun variantClick(button: View){
+         val selectedVariant = (button as Button ).text.toString().toInt()
+         if (selectedVariant== res )
+             getRightAnswer()
+         else{
+             val intent = Intent(this,second::class.java)
+             startActivity(intent)
+         }
+    }
+
+    private fun getRightAnswer() {
+        var firstNumber= random.nextInt(10 , 100)
+        val secondNumber=random.nextInt(10,100)
+        var temp = random.nextInt(0,4)
+        when(temp){
+            0 ->{
+                tvoperator.text="$firstNumber+$secondNumber"
+                res = firstNumber+secondNumber
+            }
+            1 ->{
+                tvoperator.text="$firstNumber-$secondNumber"
+                res = firstNumber-secondNumber
+            }
+            2 ->{
+                tvoperator.text="$firstNumber*$secondNumber"
+                res = firstNumber*secondNumber
+            }
+            3 ->{
+                res = random.nextInt(0,10)
+                firstNumber=res * secondNumber
+                tvoperator.text= "$firstNumber/$secondNumber"
+                res = firstNumber/secondNumber
+            }
+        }
+        val correctAnswersPosition = random.nextInt(0,4)
+        generateWrongAnswer(btn1)
+        generateWrongAnswer(btn2)
+        generateWrongAnswer(btn3)
+        generateWrongAnswer(btn4)
+        when(correctAnswersPosition){
+            0-> btn1.text=res.toString()
+            1-> btn2.text=res.toString()
+            2-> btn3.text=res.toString()
+            3-> btn4.text=res.toString()
+        }
+    }
+
+    private fun generateWrongAnswer(button: Button){
+        when(random.nextBoolean()){
+            true->
+                button.text = (res+ random.nextInt(0,10)+1).toString()
+            false-> button.text = (res - random.nextInt(0, 10) + 1).toString()
+        }
+    }
+
+
+
+
+}
---
+++ app/src/main/java/com/example/sabaq_8/second.kt
@@ -0,0 +1,26 @@
+package com.example.sabaq_8
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_second.*
+import kotlin.random.Random.Default.Companion
+
+class second : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        restart.setOnClickListener {
+            val intent = Intent (this,MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+            finish()
+        }
+        gotomain.setOnClickListener {
+            val intent = Intent(this,MainActivity2::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
